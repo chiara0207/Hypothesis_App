7cd131ed0bee2e60d0f80cde438f022e919630fe
syedfaraa94 <syedfaraa94@fu-berlin.de>
2026-07-16 22:52:15 +0200

    Fix routing and update config

---
+++ frontend-web/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Put this on the top level, not inside experimental!
+  allowedDevOrigins: ['192.168.8.106', 'localhost:3000']
+};
+
+export default nextConfig;
